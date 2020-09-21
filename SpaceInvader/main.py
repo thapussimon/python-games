efd4c7e1de53c86b9065e4c9064406a65af053fc
Thapus Simon <you@example.com>
2020-09-21 15:01:15 +0530

    bullet firing added to player

--- SpaceInvader/main.py
+++ SpaceInvader/main.py
@@ -1,7 +1,9 @@
+
+# 1.26.24 Time
+
 import pygame
 
 from random import randint
-
 
 # Initialize The pygame
 pygame.init()
@@ -31,9 +33,32 @@ playerX_change = 0
 
 
 enemyImg = pygame.image.load("enemy.png")
-enemyX = randint(50,700)
-enemyY = randint(50,150)
-enemyX_change = 0
+enemyX = randint(0, 780)
+enemyY = randint(0, 150)
+enemyX_change = 1
+enemyY_change = 30
+
+# Background Image
+
+bgImg = pygame.image.load("background.jpg")
+
+# Bullet
+bulletImg = pygame.image.load("bullet.png")
+bulletX = 0
+bulletY = 480
+bulletX_change = 0
+bulletY_change = 8
+
+# Ready state You can't see the bullet on the screen
+# Fire the bullet is moving
+bulletState = "ready"
+
+
+def fire_bullet(x, y):
+    global bulletState
+    bulletState = "fire"
+    #If the value of x & y is give directly it would be shot from the LHS, We need it to be shot from the exact middle
+    screen.blit(bulletImg, (x+16 , y+10 )) #x+16, & y+10 is found out by playing with the nos, it is used to make the bullet come from the center
 
 
 def player(x, y):
@@ -49,27 +74,29 @@ running = True
 while running:
     # This Takes RGB Values
     screen.fill((0, 0, 0))  # Fills The SCreen to The RGB Value
+    screen.blit(bgImg, (0, 0))
 
     # Checking The events, We nee to check for the window closing
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False  ##CLosing The Window, This check if the user press X, The window gets closed
 
-
-        #Player Movements Control Part
+        # Player Movements Control Part
         # If Keystroke is pressed check for left or right
         if event.type == pygame.KEYDOWN:
             print("A Keystroke is Pressed")
             if event.key == pygame.K_LEFT:
                 print("Left Arrow Pressed")
-                playerX_change = -0.3
-                playerX += playerX_change
+                playerX_change = -5
+
             if event.key == pygame.K_RIGHT:
                 print("Right Arrow Pressed")
-                playerX_change = 0.3
+                playerX_change = 5
+            if event.key==pygame.K_SPACE:
+                print("Space is pressed")
+                fire_bullet(playerX,bulletY)
 
-
-        #Player Keystroke Control Part
+        # Player Keystroke Control Part
         # Keystroke Release Checking
         if event.type == pygame.KEYUP:
             playerX_change = 0
@@ -78,22 +105,34 @@ while running:
                 print("Key stroke has been Released")
 
 
-
-
-
-
-
     playerX += playerX_change  # Left and Right Movement of The player is fixed
 
-    # Boundary CHecking To restrict The Movement
+    # Player Boundary CHecking To restrict The Movement
 
     if playerX < 0:
         playerX = 0
     elif playerX > 736:  # Since the png we have selected is 64 * 64,we have to subtract it from the 800 width which gives 736
         playerX = 736
 
+    # Calling Player to draw The player on the screen
     player(playerX, playerY)
-
+    # calling Enemy to draw The enemy on the screen
     enemy(enemyX, enemyY)
+
+    # Enemy Setup= Boundary, Movement
+    enemyX += enemyX_change
+
+    if enemyX < 0:
+        enemyX_change = 1.5
+        enemyY += enemyY_change
+
+    elif enemyX > 736:
+        enemyX_change = -1.5
+        enemyY += enemyY_change
+
+    #Bullet Movement, FOr Persistence
+    if bulletState=="fire":
+        fire_bullet(playerX,bulletY)
+        bulletY-=bulletY_change
 
     pygame.display.update()
